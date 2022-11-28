59aebf3e9e6a928116ea9eb4852d995bf0fe88e7
andreapmacedo <aa.andremacedo@gmail.com>
2022-11-28 19:52:42 -0300

    feat: Adiciona componente 'GroupCard'.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,20 @@
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+
 import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
+      <Header />
+      <Highlight 
+        title="Turmas"
+        subtitle="jogue com sua turma"
+      />
 
+      <GroupCard 
+        title="Galera do Ignite" />
     </Container>
   );
 }
